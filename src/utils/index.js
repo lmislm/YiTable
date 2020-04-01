c82e15ce09e4bab91bda15cfccd752782a31c13b
liumin <liumin@tmxmall.com>
2020-04-01 17:01:26 +0800

    feat: 新增列选中状态

--- src/utils/index.js
+++ src/utils/index.js
@@ -28,3 +28,32 @@ export function pick(object, properties) {
 export function range(from, to) {
   return [...Array(to - from)].map((_, i) => i + from)
 }
+
+export function toggleRowStatus(statusArr, row, newVal) {
+  let changed = false
+  const index = statusArr.indexOf(row)
+  const included = index !== -1
+
+  const addRow = () => {
+    statusArr.push(row)
+    changed = true
+  }
+  const removeRow = () => {
+    statusArr.splice(index, 1)
+    changed = true
+  }
+  if (typeof newVal === 'boolean') {
+    if (newVal && !included) {
+      addRow()
+    } else if (!newVal && included) {
+      removeRow()
+    }
+  } else {
+    if (included) {
+      removeRow()
+    } else {
+      addRow()
+    }
+  }
+  return changed
+}
