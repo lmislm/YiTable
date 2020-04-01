b2b909d98340151976a779f7b0523c0e50757d60
liumin <liumin@tmxmall.com>
2020-04-01 17:00:54 +0800

    styles：代码空格

--- src/classes/Row.js
+++ src/classes/Row.js
@@ -6,45 +6,34 @@ export default class Row {
     this.columns = columns
     this.index = index
   }
-
   getValue(columnName) {
     return get(this.data, columnName)
   }
-
   getColumn(columnName) {
     return this.columns.find(column => column.prop === columnName)
   }
-
   getSortableValue(columnName) {
     const dataType = this.getColumn(columnName).dataType
-
     let value = this.getValue(columnName)
-
     if (value === undefined || value === null) {
       return ''
     }
-
     if (value instanceof String) {
       value = value.toLowerCase()
     }
-
     if (dataType === 'numeric') {
       return value
     }
-
     return value.toString()
   }
   // 是否可以过滤
   getFilterableValue(columnName) {
     const value = this.getValue(columnName)
-
     if (!value) {
       return ''
     }
-
     return value.toString().toLowerCase()
   }
-
   passesFilter(filter) {
     return this.columns
       .filter(column => column.isFilterable())
