87fb258006549f2b597fdefde096528498647d8e
liumin <liumin@tmxmall.com>
2020-04-08 13:13:33 +0800

    styles(Column): 删除代码空行

--- src/classes/Column.js
+++ src/classes/Column.js
@@ -16,7 +16,6 @@ export default class Column {
       'minWidth',
       'headerClass'
     ])
-
     for (const property in properties) {
       if (Object.prototype.hasOwnProperty.call(properties, property)) {
         this[property] = columnComponent.componentInstance[property]
@@ -29,31 +28,23 @@ export default class Column {
       this.component = columnComponent
     }
   }
-
   isSortable() {
     return this.sortable
   }
-
   getSortPredicate(sortOrder, allColumns) {
-    const sortFieldName = this.getSortFieldName()
-
+    const sortFieldName = this.getSortFieldName()    
     const sortColumn = allColumns.find(column => column.prop === sortFieldName)
-
     const dataType = sortColumn.dataType
-
     if (dataType.startsWith('date') || dataType === 'numeric') {
       return (row1, row2) => {
         const value1 = row1.getSortableValue(sortFieldName)
         const value2 = row2.getSortableValue(sortFieldName)
-
         if (sortOrder === 'desc') {
           return this.fNumber(value2) < this.fNumber(value1) ? -1 : 1
         }
-
         return this.fNumber(value1) < this.fNumber(value2) ? -1 : 1
       }
     }
-
     return (row1, row2) => {
       const value1 = row1.getSortableValue(sortFieldName)
       const value2 = row2.getSortableValue(sortFieldName)
@@ -63,11 +54,9 @@ export default class Column {
       return value1.localeCompare(value2)
     }
   }
-
   fNumber (value) {
     return Number(value) || value
   }
-
   getSortFieldName() {
     return this.sortBy || this.prop
   }
