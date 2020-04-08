a38341e77891a8de0683eead162c706402089bf8
liumin <liumin@tmxmall.com>
2020-04-08 13:22:23 +0800

    styles(cell): 新增注释

--- src/components/rowCell.js
+++ src/components/rowCell.js
@@ -49,8 +49,8 @@ export default {
           isColumnIndex
             ? props.row.index + 1
             : isColumnSelection
-              ? children
-              : props.row.getValue(props.column.prop),
+            ? children // slots().default
+            : props.row.getValue(props.column.prop)
         ]
       )
     ])
