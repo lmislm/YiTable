fa11ec89575b37114941b44a5e26d1d361ce523b
liumin <liumin@tmxmall.com>
2020-04-08 13:23:02 +0800

    docs: 新增代码示例

--- src/components/Example.vue
+++ src/components/Example.vue
@@ -8,10 +8,8 @@
         :filterable="filterable"
         :data="[
         { name: '张三，法外狂徒张三', nickName: '三三', age: '23', birthday: '1993' },
-        { name: '李四', nickName: '四四', age: '32', birthday: '1992' }
-      ]"
-      >
-        <!-- { name: '王五', nickName: '五五', age: '12', birthday: '1998' },
+        { name: '李四', nickName: '四四', age: '32', birthday: '1992' },
+        { name: '王五', nickName: '五五', age: '12', birthday: '1998' },
         { name: '赵六', nickName: '六六', age: '15', birthday: '1990' },
         { name: '七', nickName: '七七', age: '27', birthday: '2003' },
         { name: '八', nickName: '八八', age: '82', birthday: '2001' },
@@ -19,16 +17,18 @@
         { name: '十', nickName: '十十', age: '10', birthday: '2010' },
         { name: '十二', nickName: '十二十二', age: '1212', birthday: '91' },
         { name: '十三', nickName: '十五十五', age: '-1', birthday: '2118' },
-        { name: '十四', nickName: '十', age: '-4', birthday: '2118' } -->
+        { name: '十四', nickName: '十', age: '-4', birthday: '2118' }
+      ]"
+      >
         <!-- <div slot="nickName">名字搜索</div> -->
-        <table-column type="index" width="55" label="序号" sortable></table-column>
+        <table-column type="index" width="55" label="序号" dataType="numeric" sortable></table-column>
         <table-column type="selection" width="55"></table-column>
         <table-column prop="name" label="名字" width="100"></table-column>
         <table-column prop="nickName" label="小名" sortable></table-column>
         <table-column prop="age" label="年龄" data-type="numeric" sortable></table-column>
         <table-column prop="birthday" label="生日" sortable></table-column>
         <table-column label="操作" :filterable="false">
-          <template slot-scope="row">{{row.name}}</template>
+          <template slot-scope="scope">{{scope.row.name}}</template>
         </table-column>
         <!-- <column-example prop="b" /> -->
       </Table>
