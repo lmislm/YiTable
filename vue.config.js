9a77dc80fefcc126726da31beea0a860622b069d
lmislm <wszdlm@gmail.com>
2020-04-11 20:17:39 +0800

    styles: 新增打包配置

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,24 @@
 const port = 8100 // dev port
 
 module.exports = {
+  productionSourceMap: false,
+  css: {
+    extract: false,
+},
   devServer: {
     port: port,
     open: true,
+    hot: true,
     overlay: {
       warnings: false,
       errors: true
+    }
+  },
+  pages: {
+    index: {
+      entry: 'lib/yi-table.umd.min.js',
+      template: 'public/index.html',
+      filename: 'index.html'
     }
   },
   chainWebpack: config => {
