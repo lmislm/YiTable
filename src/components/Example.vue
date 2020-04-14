05c2aebda3a5a76c1cfc4b0901c5bcd1589490ae
liumin <liumin@tmxmall.com>
2020-04-14 17:04:45 +0800

    docs: 新增表格列配置文档说明，cache，cacheKey

--- src/components/Example.vue
+++ src/components/Example.vue
@@ -101,7 +101,7 @@ export default {
         'formatter：它用于格式化指定列的值，接受一个Function，会传入两个参数：row和column；',
         'showColumns：待展示的列，接受每一列的prop，无prop则默认显示',
         '-----',
-        'table：data、sortBy、sortOrder、tableClass、theadClass、tbodyClass、show-header、showColumns（Array）, slot="empty-text"',
+        'table：data、sortBy、sortOrder、tableClass、theadClass、tbodyClass、show-header、showColumns（Array）, slot="empty-text"，cache(Boolean)：是否缓存表格的列配置，默认开启，会根据URL来配置，cacheKey：除了根据pathname来配置以外还可以添加key，为了实现同URL多个TABLE',
         'table-column：prop、label、hidden、sortable、dataType、cellClass、headerClass、formatter、sortBy、width、min-width',
         'toggleRowSelection、clearSelection',
         '-----',
