08b10a86cf2583e880d119541c960bb1ad49a8b2
lmislm <wszdlm@gmail.com>
2020-04-15 16:28:45 +0800

    feat: 新增svg-icon，组件内可添加自定义svg图片

---
+++ src/components/YiTableIcon.vue
@@ -0,0 +1,39 @@
+<template>
+  <svg :class="svgClass" aria-hidden="true" v-on="$listeners">
+    <use :xlink:href="iconName"/>
+  </svg>
+</template>
+
+<script>
+export default {
+  name: 'YiTableIcon',
+  props: {
+    icon: {
+      type: String,
+      required: true
+    },
+    className: {
+      type: String,
+      default: ''
+    }
+  },
+  computed: {
+    iconName () {
+      return `#icon-${this.icon}`
+    },
+    svgClass () {
+      return 'yi-table-icon' + ' ' + (this.className ? this.className : '')
+    }
+  }
+}
+
+</script>
+<style lang='scss' scoped>
+.tm-icon {
+  width: 1em;
+  height: 1em;
+  vertical-align: -0.15em;
+  fill: currentColor;
+  overflow: hidden;
+}
+</style>
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,9 @@
 const port = 8100 // dev port
+const path = require('path')
 
+function resolve(dir) {
+  return path.join(__dirname, dir)
+}
 module.exports = {
   productionSourceMap: false,
   css: {
@@ -14,7 +18,30 @@ module.exports = {
       errors: true
     }
   },
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '@': resolve('src')
+      }
+    }
+  },
   chainWebpack: config => {
     config.entry('main').add('./src/index.js')
+    // set svg-sprite-loader，支持svg
+    config.module
+      .rule('svg')
+      .exclude.add(resolve('src/icons'))
+      .end()
+    config.module
+      .rule('icons')
+      .test(/\.svg$/)
+      .include.add(resolve('src/icons'))
+      .end()
+      .use('svg-sprite-loader')
+      .loader('svg-sprite-loader')
+      .options({
+        symbolId: 'icon-[name]'
+      })
+      .end()
   }
 }
