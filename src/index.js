b544accf0deb60679503469aba9214b1ad4048ad
liumin <liumin@tmxmall.com>
2020-04-16 15:29:56 +0800

    fix: 修复组件install

--- src/index.js
+++ src/index.js
@@ -2,18 +2,12 @@ import YiTable from './components/Table'
 import YiColumn from './components/TableColumn'
 import ColumnMixin from './components/column-mixin'
 import './icons/index'
-const components = [YiTable, YiColumn, ColumnMixin]
-const install = function (Vue) {
-  if (install.installed) return
-  components.map(com => Vue.component(com.name, com))
-}
-// script直接引入文件
-if (typeof window !== 'undefined' && window.Vue) {
-  install(window.Vue)
-}
+
 export default {
-  install,
-  YiTable,
-  YiColumn,
-  ColumnMixin
+  install(Vue) {
+    Vue.component('yi-table', YiTable)
+    Vue.component('yi-table-column', YiColumn)
+    Vue.component('yi-table-icon', YiTableIcon)
+  }
 }
+export {YiTable, YiColumn, ColumnMixin}
