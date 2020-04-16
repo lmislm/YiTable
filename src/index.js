b9cb4bb40080988ea3afd24b8f4fc16f18ad9696
liumin <liumin@tmxmall.com>
2020-04-16 13:24:46 +0800

    test: 新增yi-table-icon

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import YiTable from './components/Table'
 import YiColumn from './components/TableColumn'
+import YiTableIcon from './components/YiTableIcon'
 
 import ColumnMixin from './components/column-mixin'
 
@@ -7,6 +8,7 @@ export default {
   install(Vue) {
     Vue.component('yi-table', YiTable)
     Vue.component('yi-table-column', YiColumn)
+    Vue.component('yi-table-icon', YiTableIcon)
   }
 }
 export {YiTable, YiColumn, ColumnMixin}
