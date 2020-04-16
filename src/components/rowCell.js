0dcef6d1eee49b14719a75dae6b6a45adc5500ac
liumin <liumin@tmxmall.com>
2020-04-16 14:22:39 +0800

    test: 测试rowCell文件

--- src/components/rowCell.js
+++ src/components/rowCell.js
@@ -22,44 +22,44 @@ export default {
         'min-width': parseInt(props.column.minWidth) + 'px'
       }
     }
-    // if (props.column.template) {
-    //   return createElement('td', data, [
-    //     createElement(
-    //       'span',
-    //       {
-    //         class: 'cell'
-    //       },
-    //       props.column.template({
-    //         row: props.row.data,
-    //         column: props.column,
-    //         $index: props.index
-    //       })
-    //     )
-    //   ])
-    // }
+    if (props.column.template) {
+      return createElement('td', data, [
+        createElement(
+          'span',
+          {
+            class: 'cell'
+          },
+          props.column.template({
+            row: props.row.data,
+            column: props.column,
+            $index: props.index
+          })
+        )
+      ])
+    }
     // if (props.column.component) {
     //   const component = props.column.component
     //   component.componentOptions.propsData.row = props.row.data
     //   return component
     // }
-    // // TODO: 优化Function的判断
-    // if (props.column.formatter.name !== '_default') {
-    //   data.domProps = {}
-    //   data.domProps.innerHTML = props.column.formatter(
-    //     props.row.getValue(props.column.prop),
-    //     props.row.data
-    //   )
-    // }
-    // if (props.column.selectable.name !== '_default') {
-    //   let canSelect = props.column.selectable(props.row.data, props.row.index)
-    //   if (canSelect === 'undefined') {
-    //     canSelect = true
-    //   }
-    //   props.row.isSelectable = canSelect
-    //   if (!canSelect) {
-    //     props.row.isSelected = false
-    //   }
-    // }
+    // TODO: 优化Function的判断
+    if (props.column.formatter.name !== '_default') {
+      data.domProps = {}
+      data.domProps.innerHTML = props.column.formatter(
+        props.row.getValue(props.column.prop),
+        props.row.data
+      )
+    }
+    if (props.column.selectable.name !== '_default') {
+      let canSelect = props.column.selectable(props.row.data, props.row.index)
+      if (canSelect === 'undefined') {
+        canSelect = true
+      }
+      props.row.isSelectable = canSelect
+      if (!canSelect) {
+        props.row.isSelected = false
+      }
+    }
     // 表格列类型
     const createElementValue = (function value() {
       const columnType = props.column.type
