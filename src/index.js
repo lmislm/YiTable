7afaa7ba59de3adc94309e055dd392079321ab2e
liumin <liumin@tmxmall.com>
2020-04-16 13:46:50 +0800

    fix: 修复test环境中打包svg错误

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 import YiTable from './components/Table'
 import YiColumn from './components/TableColumn'
-import YiTableIcon from './components/YiTableIcon'
-
 import ColumnMixin from './components/column-mixin'
+import './icons/index'
 
 export default {
   install(Vue) {
     Vue.component('yi-table', YiTable)
     Vue.component('yi-table-column', YiColumn)
-    Vue.component('yi-table-icon', YiTableIcon)
   }
 }
-export {YiTable, YiColumn, ColumnMixin, YiTableIcon}
+export {YiTable, YiColumn, ColumnMixin}
