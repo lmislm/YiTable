44f5483b4042fb73ce38941447afed0aabd114d7
lmislm <wszdlm@gmail.com>
2020-04-19 18:05:32 +0800

    chore: 测试去除vue实例配置

--- vue.config.js
+++ vue.config.js
@@ -25,7 +25,7 @@ module.exports = {
     resolve: {
       alias: {
         '@': resolve('src'),
-        'vue$': 'vue/dist/vue.esm.js'
+        'vue$': 'vue/dist/vue.runtime.esm.js'
       }
     },
     externals: {
