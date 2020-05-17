b0e2a75a622f08b940c32ab0d83736ec98711719
lmislm <wszdlm@gmail.com>
2020-05-17 16:27:40 +0800

    doc: 更新文档示例，添加checkbox示例

--- src/components/Example.vue
+++ src/components/Example.vue
@@ -2,26 +2,9 @@
   <div class="example">
     <div class="item table">
       <button>我是一些其他地方的按钮</button>
-      <label class="yi-checkbox">
-        <span class="yi-checkbox__input">
-          <span class="yi-checkbox__inner"></span>
-          <input type="checkbox" class="yi-checkbox__original">
-        </span>
-      </label>
-      <input type="checkbox" :indeterminate.prop="true">
-      <label class="yi-checkbox is-checked">
-        <span class="yi-checkbox__input is-checked">
-          <span class="yi-checkbox__inner"></span>
-          <input type="checkbox" class="yi-checkbox__original">
-        </span>
-      </label>
-      <input type="checkbox" :checked="true">
-      <input type="checkbox" :disabled="true">
       <Table
         :showFilter="showFilte"
         :showColumns="currentColumns"
-        border
-        stripe
         highlight-current-row
         align="left"
         ref="yitable"
@@ -31,7 +14,7 @@
         :data="isOriginalData ? mockData : dataList"
       >
         <!-- <span slot="filter-icon">筛选slot</span>
-        <span slot="option-icon">过滤slot</span> -->
+        <span slot="option-icon">过滤slot</span>-->
         <div slot="name">名字搜索</div>
         <table-column prop="index" type="index" label="序号"></table-column>
         <table-column prop="selection" type="selection" width="55" :selectable="handleSelectable"></table-column>
@@ -41,12 +24,13 @@
         <table-column prop="birthday" label="生日" sortable></table-column>
         <table-column
           label="操作"
-          prop="operate"
           :filterable="false"
           align="right"
           cellClass="cell-test"
         >
-          <template slot-scope="scope">{{scope.row.name}}</template>
+          <!-- prop="operate" -->
+          <!-- <template>111</template> -->
+          <!-- <template slot-scope="scope">{{scope.row.name}}</template> -->
         </table-column>
         <!-- <column-example prop="b" /> -->
       </Table>
@@ -89,6 +73,19 @@
         <div class="title">高亮某行</div>
         <button @click="highLightRow">高亮某行</button>
       </div>
+      <div class="content-checkbox">
+        <div class="title">checkbox样式组件</div><br>
+        <Checkbox
+          v-model="isShow"
+          :indeterminate="isIndeterminate"
+          :disabled="isDisabled"
+          :checked="isChecked"
+        >我是label</Checkbox>
+      </div>
+      check:<input type="checkbox" v-model="isChecked" />
+      disabled: <input type="checkbox" v-model="isDisabled" />
+      indeterminate: <input type="checkbox" v-model="isIndeterminate" />
+      isShow: {{isShow}}__{{isChecked}}==={{isDisabled}}___{{isIndeterminate}}
     </div>
   </div>
 </template>
@@ -97,14 +94,20 @@
 import Table from './Table.vue'
 import TableColumn from './TableColumn.vue'
 // import ColumnExample from './ColumnExample.vue'
+import Checkbox from './Checkbox.vue'
 
 export default {
   components: {
     Table,
-    TableColumn
+    TableColumn,
+    Checkbox
   },
   data () {
     return {
+      isShow: false,
+      isChecked: false,
+      isDisabled: false,
+      isIndeterminate: false,
       showFilte: false,
       isOriginalData: true,
       isHiddenName: false,
@@ -175,7 +178,7 @@ export default {
         return true
       }
     },
-    toggleSelection(rows) {
+    toggleSelection (rows) {
       if (rows) {
         rows.forEach(row => {
           this.$refs.yitable.toggleRowSelection(row)
@@ -187,7 +190,7 @@ export default {
     toggleAllSelection () {
       this.$refs.yitable.toggleAllSelection()
     },
-    rowClassName ({row, index}) {
+    rowClassName ({ row, index }) {
       console.log(row, 'rowClassName')
       if (index === 2) {
         return 'test-row'
@@ -200,16 +203,6 @@ export default {
 }
 </script>
 <style lang="scss">
-$--checkbox-font-color: #606266;
-$--checkbox-font-weight: 500;
-$--font-size-base: 14px;
-$--checkbox-border-radius: 2px;
-$--checkbox-input-width: 14px;
-$--checkbox-input-height: 14px;
-$--checkbox-background-color: #FFFFFF;
-$--index-normal: 1;
-$--checkbox-input-border-color-hover: #409EFF;
-$--checkbox-checked-icon-color: #FFFFFF;
 .example {
   display: flex;
   flex-direction: row;
@@ -223,67 +216,10 @@ $--checkbox-checked-icon-color: #FFFFFF;
   .test-row {
     background-color: oldlace;
   }
-  .yi-checkbox {
-    color: $--checkbox-font-color;
-    font-weight: $--checkbox-font-weight;
-    font-size: $--font-size-base;
-    position: relative;
-    cursor: pointer;
-    display: inline-block;
-    white-space: nowrap;
-    user-select: none;
-    margin-right: 0px;
-    .yi-checkbox__input {
-      white-space: nowrap;
-      cursor: pointer;
-      outline: none;
-      display: inline-block;
-      line-height: 1;
-      position: relative;
-      vertical-align: middle;
-      .yi-checkbox__inner {
-        display: inline-block;
-        position: relative;
-        border: 1px solid #dcdfe6;
-        border-radius: $--checkbox-border-radius;
-        box-sizing: border-box;
-        width: $--checkbox-input-width;
-        height: $--checkbox-input-height;
-        background-color: $--checkbox-background-color;
-        z-index: $--index-normal;
-        transition: border-color .25s cubic-bezier(.71,-.46,.29,1.46),
-        background-color .25s cubic-bezier(.71,-.46,.29,1.46);
-
-        &:hover {
-          border-color: $--checkbox-input-border-color-hover;
-        }
-
-        &::after {
-          box-sizing: content-box;
-          content: "";
-          border: 1px solid $--checkbox-checked-icon-color;
-          border-left: 0;
-          border-top: 0;
-          height: 7px;
-          left: 4px;
-          position: absolute;
-          top: 1px;
-          transform: rotate(45deg) scaleY(0);
-          width: 3px;
-          transition: transform .15s ease-in .05s;
-          transform-origin: center;
-        }
-      }
-      .yi-checkbox__original {
-        opacity: 0;
-        outline: none;
-        position: absolute;
-        margin: 0;
-        width: 0;
-        height: 0;
-        z-index: -1;
-      }
-    }
+  .content-checkbox {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
   }
 }
 .title {
